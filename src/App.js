c0feaf388b1da044c83fe13dc93a30bcceeef4e6
hossein-nahali <hosseinnahalidev@outlook.com>
2023-02-22 15:30:06 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import {GeneralSettingTheme} from "./DesignSystem/GeneralSettingTheme";
+import {ControlsDirection} from "./DesignSystem/Components/ControlsDirection/Index";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    GeneralSettingTheme()
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ControlsDirection type={'mobile'} direction={'right'}/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
