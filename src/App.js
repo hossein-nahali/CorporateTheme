f7fe98fc7ffca70be3a5565e3a6d1b8828107a21
hossein-nahali <hosseinnahalidev@outlook.com>
2023-02-25 19:47:35 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.scss';
 import {GeneralSettingTheme} from "./DesignSystem/GeneralSettingTheme";
 import {ControlsDirection} from "./DesignSystem/Components/ControlsDirection/Index";
+
 function App() {
     GeneralSettingTheme()
 
     return (
         <div className="App">
             <header className="App-header">
-                <ControlsDirection type={'mobile'} direction={'right'}/>
+                <ControlsDirection type={'desktop'} direction={'right'}/>
             </header>
         </div>
     );
