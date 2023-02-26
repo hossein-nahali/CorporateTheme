8c6acbce86dd4d994c584c7d5fea78b6db67bbfb
hossein-nahali <hosseinnahalidev@outlook.com>
2023-02-26 19:43:54 +0330

    Update Index.js

--- src/GeneralState/Index.js
+++ src/GeneralState/Index.js
@@ -1,5 +1,5 @@
 // clean export contexts
-export {GeneralSettingThemeContext} from './Context/GeneralSettingThemeContext'
+export {default as GeneralSettingThemeContext} from './Context/GeneralSettingThemeContext'
 
 // clean export reducers
 export {default as GeneralSettingThemeReducer} from './Reducer/GeneralSettingThemeReducer'
